da95a725a8255a9abab607af61081efaeffbce42
surajkoju <51220236+SurajKoju@users.noreply.github.com>
2023-11-01 17:45:35 +0545

    Fixed Top rated API Issue, Fixed design Issue on company logo

--- src/constants/apiConstants.ts
+++ src/constants/apiConstants.ts
@@ -3,4 +3,4 @@ const API = import.meta.env.VITE_API;
 
 export const TRENDING_TODAY = `${API}trending/movie/day?api_key=${API_KEY}&language=en-us`;
 export const TRENDING_WEEK = `${API}trending/movie/week?api_key=${API_KEY}&language=en-us`;
-export const TOP_RATED = `${API}movie/top_rated/?api_key=${API_KEY}&language=en-us`;
+export const TOP_RATED = `${API}movie/top_rated?api_key=${API_KEY}&language=en-us`;
